da4b936304bd8997b8de8dee8650759a40db35eb
Neha-081 <nehaprajapati1150@gmail.com>
2022-03-29 15:54:41 +0530

    assignment

--- another.js
+++ another.js
@@ -1,6 +1,8 @@
 
 
 //Another Function
+
+var startTime=performance.now()
 class Trip2 {
     constructor(group2) {
         this.neha2 = group2;
@@ -32,9 +34,10 @@ Trip2.prototype.addMember2 = function (nameArray) {
 
 //Function to add Expenses
 
-Trip2.prototype.addExpenses2 = function (expenses) {
+Trip2.prototype.addExpenses2 = function () {
     for (var i = 0; i < this.neha2.length; i++) {
-        this.neha2[i].expenses = expenses
+        let randomNum =300*(Math.random())
+        this.neha2[i].expenses = Math.trunc(randomNum)
     }
     return JSON.stringify(this.neha2, null, 4)
 }
@@ -55,7 +58,7 @@ Trip2.prototype.showSplitData2 = function () {
 
 
 
-
+var endTime=performance.now()
 
 
 // Create Trip2 Group2
@@ -70,12 +73,15 @@ console.log("Added members to the group2 =", trip2);
 
 
 //Adding expenses
-var trip2 = createTrip2.addExpenses2(30)
+
+var trip2 = createTrip2.addExpenses2()
 console.log("Added 30 rs to the group2 with same members =", trip2);
 
 // Final output
 var trip2 = createTrip2.showSplitData2()
+let totalTime=endTime-startTime
 console.log("Final Output =", trip2);
+console.log("Total Time=",totalTime);
 
 
 
